4b2207f716c7181c11fcfd1b2eb8da4b40b72e73
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2020-12-14 20:58:44 +0100

    DidConf pipeline.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # didconf
 Website for didconf.com
+
+Run locally:
+
+`$ php -S localhost:8000`
---
+++ public/index.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+<title>A Dutch iOS developer conference by AppForce 1 - Jeroen Leenarts</title>
+</head>
+<body>
+<center>
+  <h1>Dutch iOS Developer Conference</h1>
+<p>More info soon.</p>
+<p>Dutch iOS Developer Conference says: "Hello world!"</p>
+<p>D could just as well stand for distributed, diverse, dazzling.</p>
+<p>Working title contains Dutch, cause it can.</p>
+<img src="https://appforce1.net/assets/logo.png" width=100%>
+</center>
+</body>
+</html>
